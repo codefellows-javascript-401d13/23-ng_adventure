70fe45aee52a5bc9a4f942eed112d61ff4851857
caylazabel <caylazabel@gmail.com>
2017-03-24 13:15:30 -0700

    included require statements in entry.js

--- app/entry.js
+++ app/entry.js
@@ -6,4 +6,10 @@ const angular = require('angular');
 
 angular.module('ngAdventure', []);
 
-// require('')
+
+require('./service/map-service.js');
+require('./service/player-service.js');
+
+require('./component/game-pad/game-pad.js');
+require('./component/player-info/player-info.js');
+require('./component/game-history/game-history.js');
