30c3558bbd055bce3ee675edf382e904446d3b05
Anna Ulanova <anetteua0@gmail.com>
2017-03-26 12:54:49 -0700

    map

--- lab-anna/app/service/map-service.js
+++ lab-anna/app/service/map-service.js
@@ -12,36 +12,49 @@ function mapService($log) {
   service.mapData = {
     web: {
       desc: "Wake up sleepy head, go get the fly!",
-      right: "enterSpout"
+      right: "enterSpout",
+      x: "5vw",
+      y: "25vw"
     },
     ground:{
       desc: "Hurry up! The rain is coming!",
       left: "enterSpout",
-      right: "enterSpout",
+      right: "sunnySide1",
       up: "enterSpout",
-      down: "enterSpout"
+      x: "12",
+      y: "30"
     },
     enterSpout: {
       desc: "Enter the spout or go around.",
       left: "ground",
-      right: "sunnySide1"
+      right: "sunnySide1",
+      x: "15",
+      y: "25"
     },
     sunnySide1: {
       desc: "You are on the right track, keep going!",
       left: "ground",
-      up: "sunnySide2"
+      up: "sunnySide2",
+      x: "25",
+      y: "25"
     },
     sunnySide2: {
       desc: "You are on the right track, keep going!",
       left: "ground",
-      up: "roof"
+      up: "roof",
+      x: 25,
+      y: 15
     },
     roof: {
       desc: "Yay! You climbed up onto the roof! Now find the fly.",
-      right: "fly"
+      right: "fly",
+      x: "25",
+      y: "3"
     },
     fly: {
-      desc: "You have found the fly, bon appetit!"
+      desc: "You have found the fly, bon appetit!",
+      x: "31",
+      y: "1"
     }
   };
   return service;
