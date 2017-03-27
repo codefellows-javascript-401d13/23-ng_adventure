0e83be6ad7168775f16037f135c8572848e31aab
Jeremiah <jeremiaht.walters@gmail.com>
2017-03-26 17:53:27 -0700

    finished map

--- app/service/map-service.js
+++ app/service/map-service.js
@@ -35,7 +35,8 @@ function mapService($log) {
     gym: {
       desc: 'zombie P.E. students run toward you chanting ring around the rosies!',
       west: 'hallway',
-      east: 'playground'
+      east: 'playground',
+      north: 'gym'
     },
     playground:{
       desc: 'your on the playground where there is a little girl wearing bloody clothes sitting on the swing with her head down...she notices you',
@@ -47,16 +48,30 @@ function mapService($log) {
     swingset:{
       desc: 'you got too close! the little girl bites you with her jagged zombie teeth and digs her nails into your back!',
       south: 'ground',
+      north: 'corridor',
+      west: 'playground',
+      east: 'ballpit'
+    },
+    ballpit:{
+      desc: 'you are stuck',
+      south: 'ballpit',
+      north: 'ballpit',
+      west: 'ballpit',
+      east: 'ballpit'
     },
     ground:{
       desc: 'your on the ground wrestling the zombie girl to get her off of you. You have several zombie bites.  It may be too late if you cant get to the lunchroom in two moves',
       north: 'swingset',
-      south: 'corridor'
+      south: 'ballpit',
+      east: 'playground',
+      west:'corridor',
     },
     corridor: {
       desc: '',
       north: 'playground',
-      east: 'lunchroom'
+      east: 'lunchroom',
+      west: 'office',
+      south: 'lunchroom'
     },
     lunchroom: {
       desc: 'you have eaten some cool snacks and your not going to be a zombie...maybe',
@@ -65,7 +80,11 @@ function mapService($log) {
       north: 'outside'
     },
     outside: {
-      desc: 'you made it out of the Dark and Dead school ',
+      desc: 'you made it out of the Dark and Dead school.  You are lucky you survived.  Never return!',
+      south: 'outside',
+      north: 'outside',
+      west: 'outside',
+      east: 'outside'
 
     }
   };
