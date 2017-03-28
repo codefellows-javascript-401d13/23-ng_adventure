874f1a544623f399890a7cfb2437e279991d5796
GLSea1979 <builtbygl@gmail.com>
2017-03-27 17:14:54 -0700

    game plays ok

--- app/service/player-service.js
+++ app/service/player-service.js
@@ -11,18 +11,18 @@ function playerService($q, $log, mapService) {
   let service = {};
 
   let turn = 0;
+  let key = 0;
   let player = service.player = {
     name: 'Gary',
     location: 'Hall',
-    hp: 16
   };
 
   let history = service.history = [
     {
       turn,
-      desc: 'Welcome to Garys Adventure',
-      location: player.location,
-      hp: player.hp
+      story: 'Gary enters the old house quietly and with a bit of a shudder. The door slams shut behind him, he checks the door it is locked. In order for him to leave he will need to find the key.',
+      desc: 'There is a door to the right and left.',
+      location: player.location
     }
   ];
 
@@ -36,24 +36,39 @@ function playerService($q, $log, mapService) {
       if (!newLocation) {
         history.unshift({
           turn,
+          story: 'um, just like before except now you are sore because, ',
           desc: 'you have hit a wall!!!',
-          location: player.location,
-          hp: player.hp
+          location: player.location
         });
         return reject('no room in that direction');
-      };
+      }
+      $log.debug('in', player.location, 'key', key);
+      if (player.location === 'Hall' && key === 1) {
+        mapService.mapData[newLocation].story = 'Gary rushes to the locked door, turns the key and escapes.  Win!!!!!!!!!';
+
+      }
+
+
 
       history.unshift({
         turn,
+        story: mapService.mapData[newLocation].story,
         location: player.location,
-        desc: mapService.mapData[newLocation].desc,
-        hp: player.hp
+        desc: mapService.mapData[newLocation].desc
       });
 
       player.location = newLocation;
+
+
+      if (player.location === 'attic' && key !== 1) {
+        key = 1;
+        mapService.mapData[newLocation].story = 'Been here before, looks like Gary is lost.';
+        $log.debug('you found the key', key);
+      }
+
       return resolve(player.location);
     });
   };
 
   return service;
-};
+}
